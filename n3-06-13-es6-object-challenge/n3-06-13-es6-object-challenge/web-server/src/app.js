d298b62e0c7bd4bd365599f0fc82bc75a7f64d0e
Dhananjaya <dhananjayakrd@gmail.com>
2022-12-16 17:19:41 +0530

    handlebars and 404 page

--- n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/web-server/src/app.js
+++ n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/web-server/src/app.js
@@ -1,26 +1,61 @@
+const path = require('path')
 const express = require("express");
-
-
-console.log(__dirname)
-console.log(__filename)
+const hbs = require('hbs')
 
 const app = express()
 
-app.get("", (req, res) => {
-  res.send("<h1>Weather</h1>")
-});
+// define paths for express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-app.get("/help", (req, res) => {
-  res.send("Help page!");
-});
+// setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
-app.get("/about", (req, res) => {
-  res.send("About");
-});
+// setup static directory to serve
+app.use(express.static(publicDirPath))
+
+app.get('', (req, res)=>{
+   res.render('index', {
+    title: 'weather app',
+    name: 'dhananjaya'
+   })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+      title: 'About Me',
+      name: 'Andrew Mead'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+      helpText: 'This is some helpful text.',
+      title: 'Help',
+      name: 'Andrew Mead'
+  })
+})
 
 app.get("/weather", (req, res) => {
   res.send("Your weather");
 });
+
+app.get('/help/*', (req, res)=>{
+   res.render('404',{
+    errorMessage: 'Help article not found'
+   })
+})
+
+app.get('*', (req, res)=>{
+  res.render('404', {
+    title: '404',
+    name: 'Andrew Mead',
+    errorMessage: 'page not found'
+  })
+})
 
 app.listen(3000, () => {
   console.log("server is up on port 3000.");
