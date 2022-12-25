e26f6bc211c4034873221c9bfd69c27a44046071
Dhananjaya <dhananjayakrd@gmail.com>
2022-12-26 00:12:41 +0530

    fetch when age is not there

--- n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/task-manager/mongodb.js
+++ n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/task-manager/mongodb.js
@@ -6,7 +6,6 @@ const { MongoClient, ObjectID } = require("mongodb");
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
-
 MongoClient.connect(
   connectionURL,
   { useNewUrlParser: true },
@@ -17,12 +16,12 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection('users').findOne({ name: 'jen' }, (error, user)=>{
-        if (error) {
-            return console.log('unable to fetch')
-        }
-        console.log(user)
-    })
+    db.collection("users").findOne({ name: "jen", age: 1 }, (error, user) => {
+      if (error) {
+        return console.log("unable to fetch");
+      }
+      console.log(user);
+    });
 
     // db.collection("users").insertOne(
     //   {
