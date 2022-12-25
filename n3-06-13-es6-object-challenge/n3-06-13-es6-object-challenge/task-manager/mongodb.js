11cb8da058197798403fc0ff17136b1b447ca9f3
Dhananjaya <dhananjayakrd@gmail.com>
2022-12-26 00:09:19 +0530

    valid operation for fetching document with name

--- n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/task-manager/mongodb.js
+++ n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/task-manager/mongodb.js
@@ -6,9 +6,6 @@ const { MongoClient, ObjectID } = require("mongodb");
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
-const id = new ObjectID();
-console.log("dhanu-dev", id);
-console.log("dhanu-dev", id.getTimestamp());
 
 MongoClient.connect(
   connectionURL,
@@ -20,20 +17,27 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne(
-      {
-        _id: id,
-        name: "Vikram",
-        age: 27,
-      },
-      (error, result) => {
+    db.collection('users').findOne({ name: 'jen' }, (error, user)=>{
         if (error) {
-          return console.log("Unable to insert user");
+            return console.log('unable to fetch')
         }
+        console.log(user)
+    })
 
-        console.log("here is reult", result.insertedIds);
-      }
-    );
+    // db.collection("users").insertOne(
+    //   {
+    //     _id: id,
+    //     name: "Vikram",
+    //     age: 27,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert user");
+    //     }
+
+    //     console.log("here is reult", result.insertedId);
+    //   }
+    // );
 
     // db.collection('users').insertMany([
     //     {
