66c383cbe82ab9a5b29c469c17f629e4b9635c78
Dhananjaya <dhananjayakrd@gmail.com>
2022-12-26 17:26:15 +0530

    settingup-mongoose

--- n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/task-manager/src/db/mongoose.js
+++ n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/task-manager/src/db/mongoose.js
@@ -1,15 +1,28 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-    useNewUrlParser: true,
-    useCreateIndex: true
-})
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api-test", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 
-const User = mongoose.model('User',{
-    name: {
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
 
-    },
-    age: {
-        
-    }
-})
+const me = new User({
+  name: "Andrew",
+  age: 27,
+});
+
+me.save()
+  .then(() => {
+     console.log('success',me)
+  })
+  .catch((err) => {
+     console.log('error',err)
+  });
