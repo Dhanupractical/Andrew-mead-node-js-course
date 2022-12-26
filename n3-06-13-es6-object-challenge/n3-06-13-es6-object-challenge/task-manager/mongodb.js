a668de672423e49ad9c26c1336a975ce91565c8a
Dhananjaya <dhananjayakrd@gmail.com>
2022-12-26 15:34:08 +0530

    deleteMany

--- n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/task-manager/mongodb.js
+++ n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/task-manager/mongodb.js
@@ -113,19 +113,30 @@ MongoClient.connect(
     //   console.log('error',error)
     // })
 
-    db.collection("tasks")
-      .updateMany(
-        {
-          completed: false,
-        },
-        {
-          $set: {
-            completed: true,
-          },
-        }
-      )
+    // db.collection("tasks")
+    //   .updateMany(
+    //     {
+    //       completed: false,
+    //     },
+    //     {
+    //       $set: {
+    //         completed: true,
+    //       },
+    //     }
+    //   )
+    //   .then((result) => {
+    //     console.log(result.modifiedCount);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    db.collection("users")
+      .deleteMany({
+        age: 27,
+      })
       .then((result) => {
-        console.log(result.modifiedCount);
+        console.log(result);
       })
       .catch((error) => {
         console.log(error);
