fe0491304fc5035c1ab7a663c9bec069cbef7361
Dhananjaya <dhananjayakrd@gmail.com>
2022-12-27 14:01:33 +0530

    model with custom validation, make it fail

--- n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/task-manager/src/db/mongoose.js
+++ n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/task-manager/src/db/mongoose.js
@@ -5,48 +5,28 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api-test", {
   useCreateIndex: true,
 });
 
-// const User = mongoose.model("User", {
-//   name: {
-//     type: String,
-//   },
-//   age: {
-//     type: Number,
-//   },
-// });
-
-// const me = new User({
-//   name: "Andrew",
-//   age: 27,
-// });
-
-// me.save()
-//   .then(() => {
-//      console.log('success',me)
-//   })
-//   .catch((err) => {
-//      console.log('error',err)
-//   });
-
-const Task = mongoose.model("Task", {
-  // capital T in Task gets converted into small case t by mongoose
-  decription: {
+const UserNew = mongoose.model('UserNew', {
+  name: {
     type: String,
+    required: true
   },
-  completed: {
-    type: Boolean,
-  },
-});
+  age: {
+    type: Number,
+    validate(value) {
+      if (value < 0) {
+          throw new Error('Age must be a positive number')
+      }
+    }
+  }
+})
 
-const task = new Task({
-  decription: "Learn the Mongoose Library",
-  completed: false,
-});
+const meNew = new UserNew({
+  name: 'Dhananjaya',
+  age: -1
+})
 
-task
-  .save()
-  .then(() => {
-    console.log(task);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+meNew.save().then(()=>{
+  console.log(me)
+}).catch((err)=>{
+  console.log(err)
+})
