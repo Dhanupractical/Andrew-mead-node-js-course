8950971782a3b380a54d013e5c34b35ec0772afe
Dhananjaya <dhananjayakrd@gmail.com>
2022-12-27 13:48:53 +0530

    adding new collection and document model

--- n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/task-manager/src/db/mongoose.js
+++ n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/task-manager/src/db/mongoose.js
@@ -5,24 +5,48 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api-test", {
   useCreateIndex: true,
 });
 
-const User = mongoose.model("User", {
-  name: {
+// const User = mongoose.model("User", {
+//   name: {
+//     type: String,
+//   },
+//   age: {
+//     type: Number,
+//   },
+// });
+
+// const me = new User({
+//   name: "Andrew",
+//   age: 27,
+// });
+
+// me.save()
+//   .then(() => {
+//      console.log('success',me)
+//   })
+//   .catch((err) => {
+//      console.log('error',err)
+//   });
+
+const Task = mongoose.model("Task", {
+  // capital T in Task gets converted into small case t by mongoose
+  decription: {
     type: String,
   },
-  age: {
-    type: Number,
+  completed: {
+    type: Boolean,
   },
 });
 
-const me = new User({
-  name: "Andrew",
-  age: 27,
+const task = new Task({
+  decription: "Learn the Mongoose Library",
+  completed: false,
 });
 
-me.save()
+task
+  .save()
   .then(() => {
-     console.log('success',me)
+    console.log(task);
   })
   .catch((err) => {
-     console.log('error',err)
+    console.log(err);
   });
