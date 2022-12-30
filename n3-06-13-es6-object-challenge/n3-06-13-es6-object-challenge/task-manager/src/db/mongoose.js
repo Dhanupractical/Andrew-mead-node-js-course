130f6de0cdf92f924add9edc26fd1abbcfe9db11
Dhananjaya <dhananjayakrd@gmail.com>
2022-12-31 02:46:38 +0530

    setting up of basic REST API setup

--- n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/task-manager/src/db/mongoose.js
+++ n3-06-13-es6-object-challenge/n3-06-13-es6-object-challenge/task-manager/src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api-test", {
   useNewUrlParser: true,
@@ -7,56 +6,3 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api-test", {
   useUnifiedTopology: true,
 });
 
-const UserNew = mongoose.model("UserNew", {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 7,
-    trim: true,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error('Password cannot contain "password');
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive number");
-      }
-    },
-  },
-});
-
-const meNew = new UserNew({
-  name: "   Dhananjaya   ",
-  email: "mike@gmail.com   ",
-  password: "phone098!",
-});
-
-meNew
-  .save()
-  .then(() => {
-    console.log(meNew);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
